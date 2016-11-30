8899e8cce4f9fbd5492c2b6a89fd3da3da57ed75
tomper00 <tomas.persson@wunderkraut.com>
2016-11-30 18:08:25 +0100

    Antager fantastic push

--- templates/components/MyPages/MyPages.js
+++ templates/components/MyPages/MyPages.js
@@ -5,8 +5,62 @@ $(".js-MyPages-toggle-phone").click(function() {
   $(".js-MyPages-help-box--phone").slideToggle()
 })
 /////////////////////////////////////////////
-// Change Password POPUP
+// Change Password Modal
 /////////////////////////////////////////////
 $(".js-MyPages-password-btn").click(function() {
-  console.log("hejsan");
+  $(".js-Site-container").toggleClass("locked");
+  $(".js-MyPages-pswrd-overlay-container").fadeIn("fast");
 })
+$(".js-MyPages-close-form, .js-MyPages-pswrd-clickarea").click(function() {
+  $(".js-Site-container").toggleClass("locked");
+  $(".js-MyPages-pswrd-overlay-container").fadeOut("fast");
+})
+var $newPassword = $(".js-MyPages-new-pswrd");
+var $repeatPassword = $(".js-MyPages-repeat-pswrd");
+var $newPasswordStatus = $(".js-MyPages-new-pswrd-status");
+var $newPasswordMatch = $(".js-MyPages-new-pswrd-match");
+var passwordState = {
+  length: false,
+  password: "",
+  repeatPassword: ""
+}
+// Check if password is sufficient
+$newPassword.on('input', function() {
+ passwordState.password = $(this).val();
+ passwordState.repeatPassword = $repeatPassword.val();
+if(passwordState.password.length >= 6) {
+  passwordState.length = true;
+} else {
+  passwordState.length = false;
+}
+checkPassword()
+});
+$repeatPassword.on('input', function() {
+  passwordState.password = $newPassword.val();
+  passwordState.repeatPassword = $(this).val();
+checkPassword()
+});
+function checkPassword() {
+  if(passwordState.length) {
+    $newPasswordStatus.html("Lösenord är ok")
+    $newPasswordStatus.addClass("u-green")
+    $newPasswordStatus.removeClass("u-red")
+  } else {
+    $newPasswordStatus.html("Lösenord för kort")
+    $newPasswordStatus.removeClass("u-green")
+    $newPasswordStatus.addClass("u-red")
+  }
+  if(passwordState.password === passwordState.repeatPassword && passwordState.length) {
+    $newPasswordMatch.html("Lösenorden överenstämmer")
+    $newPasswordMatch.addClass("u-green")
+    $newPasswordMatch.removeClass("u-red")
+  } else if(passwordState.password === passwordState.repeatPassword && passwordState.length === false) {
+    $newPasswordMatch.html("Lösenorden överenstämmer men är för korta")
+    $newPasswordMatch.removeClass("u-green")
+    $newPasswordMatch.addClass("u-red")
+  } else {
+    $newPasswordMatch.html("Lösenorden överenstämmer inte")
+    $newPasswordMatch.removeClass("u-green")
+    $newPasswordMatch.addClass("u-red")
+  }
+}
