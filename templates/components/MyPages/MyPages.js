3a547391dcbbe110c6791240dfde7894f0c673e0
tomper00 <tomas.persson@wunderkraut.com>
2016-11-30 10:58:11 +0100

    Senaste + ändrade bildsökvägar

--- templates/components/MyPages/MyPages.js
+++ templates/components/MyPages/MyPages.js
@@ -3,5 +3,10 @@ $(".js-MyPages-toggle-mail").click(function() {
 })
 $(".js-MyPages-toggle-phone").click(function() {
   $(".js-MyPages-help-box--phone").slideToggle()
-
+})
+/////////////////////////////////////////////
+// Change Password POPUP
+/////////////////////////////////////////////
+$(".js-MyPages-password-btn").click(function() {
+  console.log("hejsan");
 })
