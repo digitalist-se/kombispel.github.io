b736d602009b92153b1938a96cf51e321b687d55
tomper00 <tomas.persson@wunderkraut.com>
2016-12-08 11:31:38 +0100

    simplify cookie logic

--- js/main.js
+++ js/main.js
@@ -215,23 +215,19 @@ $(".js-Login-form-submit-btn").click(function(e) {
   }
 });
 
-var cookies = document.cookie.split(';');
-var $cookieName = "_ga";
-var cookieSet = false;
+
 /////////////////////////////////////////////
 // Check if Cookie is set
 /////////////////////////////////////////////
-if(cookieSet != true) {
-  $(".Cookie-container").show()
+if (document.cookie.indexOf("_ga") >= 0) {
+   $(".Cookie-container").show();
 }
+
+
 /////////////////////////////////////////////
-// Approve and set cookie on click
+// Approve hide cookie info
 /////////////////////////////////////////////
 $(".js-Cookies-approve-btn").click(function() {
-   //var d = new Date();
-   //d.setTime(d.getTime() + (365*24*60*60*1000)); // Valid for a year
-   //var expires = "expires="+ d.toUTCString();
-   //document.cookie =$cookieName+"=true;"+expires;
    $(".Cookie-container").fadeOut("fast");
 })
 
--- templates/components/Cookies/Cookies.js
+++ templates/components/Cookies/Cookies.js
@@ -1,19 +1,12 @@
-var cookies = document.cookie.split(';');
-var $cookieName = "_ga";
-var cookieSet = false;
 /////////////////////////////////////////////
 // Check if GA Cookie is set
 /////////////////////////////////////////////
-if(cookieSet != true) {
-  $(".Cookie-container").show()
+if (document.cookie.indexOf("_ga") >= 0) {
+   $(".Cookie-container").show();
 }
 /////////////////////////////////////////////
 // Approve and hide message
 /////////////////////////////////////////////
 $(".js-Cookies-approve-btn").click(function() {
-   //var d = new Date();
-   //d.setTime(d.getTime() + (365*24*60*60*1000)); // Valid for a year
-   //var expires = "expires="+ d.toUTCString();
-   //document.cookie =$cookieName+"=true;"+expires;
-   $(".Cookie-container").fadeOut("fast");
+    $(".Cookie-container").fadeOut("fast");
 })
